267be2c2949c29cbac5c7b98fe1f8fc5ca2a4215
a. octavia <auliaoctaviani77@student.uns.ac.id>
2023-08-01 15:26:07 +0700

    Time: 289 ms (100%), Space: 152.5 MB (97.22%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.dart
+++ 0006-zigzag-conversion/0006-zigzag-conversion.dart
@@ -1,28 +1,21 @@
 class Solution {
   String? convert(String s, int numRows) {
-      bool isDcreamint=false;
-      int i=0;
-      List<List<String>> strlLists=List.generate(numRows, (index) => []);
-      String finals='';
-      if(numRows==1||s.length==1) finals=s;
-      else{
-      while(i!=s.length-1){
-        for(int j=0;j<numRows;isDcreamint?j--:j++){
-          print(j);
-          if(j==numRows-1)isDcreamint=true;
-          if(j==0)isDcreamint=false;
-          strlLists[j].add(s[i]);
-          if(i==s.length-1){
-            break;
-          }
-          i++;
-        }
+    if (numRows == 1 || s.length == 1) return s;
+
+    List<String> rows = List.generate(numRows, (_) => '');
+    int currentRow = 0;
+    bool isDownward = false;
+
+    for (int i = 0; i < s.length; i++) {
+      rows[currentRow] += s[i];
+
+      if (currentRow == 0 || currentRow == numRows - 1) {
+        isDownward = !isDownward;
       }
-      for(int k=0;k<strlLists.length;k++){
-        for(int j=0;j<strlLists[k].length;j++){
-          finals='$finals'+'${strlLists[k][j]}';
-        }
-      }}
-      return finals;
+
+      currentRow += isDownward ? 1 : -1;
+    }
+
+    return rows.join('');
   }
 }
