66bb7dd32d12a6e5f35dd1336b56097b7202c42f
a. octavia <auliaoctaviani77@student.uns.ac.id>
2023-08-03 11:49:28 +0700

    Time: 2 ms (54.39%), Space: 2 MB (10.4%) - LeetHub

--- 0050-powx-n/0050-powx-n.go
+++ 0050-powx-n/0050-powx-n.go
@@ -1,18 +1,38 @@
+// func myPow(x float64, n int) float64 {
+//     if n == 0{
+//         return 1
+//     }
+//     if n < 0 {
+//         return 1/myPow(x,-n)
+//     }
+//     temporary:=1.0
+//     for n != 0 {
+//         if n&1 == 1{
+//             temporary*=x
+//         }
+//         x*=x
+//         n>>=1
+//     }
+//     return temporary
+    
+// }
+
 func myPow(x float64, n int) float64 {
-    if n == 0{
+    if n == 0 {
         return 1
     }
+
     if n < 0 {
-        return 1/myPow(x,-n)
+        return 1 / myPow(x, -n)
     }
-    temporary:=1.0
-    for n != 0 {
-        if n&1 == 1{
-            temporary*=x
+
+    result := 1.0
+    for n > 0 {
+        if n&1 == 1 {
+            result *= x
         }
-        x*=x
-        n>>=1
+        x *= x
+        n >>= 1
     }
-    return temporary
-    
+    return result
 }
