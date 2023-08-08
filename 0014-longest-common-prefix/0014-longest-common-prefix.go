1290eb5c64f65f07b1a770e3d4f21fce6e3895f0
a. octavia <auliaoctaviani77@student.uns.ac.id>
2023-08-08 13:24:33 +0700

    Time: 0 ms (100%), Space: 2.5 MB (11.65%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.go
@@ -0,0 +1,10 @@
+func longestCommonPrefix(s []string) string {
+	pref := s[0]
+	for i := 1; i < len(s); i++ {
+		for !strings.HasPrefix(s[i], pref) {
+			pref = pref[:len(pref)-1]
+		}
+		fmt.Println(s[i])
+	}
+	return pref
+}
