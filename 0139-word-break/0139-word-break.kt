c9c782ddb5c466bcfbf35fb844f2b7eea3ba3450
a. octavia <auliaoctaviani77@student.uns.ac.id>
2023-08-12 20:35:33 +0700

    Time: 195 ms (12.29%), Space: 41.3 MB (5.44%) - LeetHub

--- 0139-word-break/0139-word-break.kt
+++ 0139-word-break/0139-word-break.kt
@@ -1,23 +1,26 @@
 class Solution {
     private lateinit var s: String
     private lateinit var wordDict: Set<String>
-    private lateinit var memo: Array<Boolean>
+    private lateinit var memo: Array<MutableList<Boolean?>>
 
     fun wordBreak(s: String, wordDict: List<String>): Boolean {
         this.s = s
         this.wordDict = wordDict.toHashSet()
-        memo = Array(s.length + 1) { false }
-        memo[s.length] = true
+        memo = Array(s.length+1) { MutableList<Boolean?>(s.length+1) { null } }
+        memo[s.length][s.length] = true
+        return dfs(0, s.length)
+    }
 
-        for (i in s.length - 1 downTo 0) {
-            for (j in i + 1..s.length) {
-                if (exists(s.substring(i, j)) && memo[j]) {
-                    memo[i] = true
-                    break
-                }
+    private fun dfs(i: Int, j: Int): Boolean {
+        if (i > j) return false
+        return memo[i][j] ?: run {
+            memo[i][j] = if (!exists(s.substring(i, j))) {
+                dfs(i, j-1)
+            } else {
+                dfs(j, s.length) || dfs(i, j-1)
             }
+            memo[i][j] ?: false
         }
-        return memo[0]
     }
 
     private fun exists(str: String): Boolean {
