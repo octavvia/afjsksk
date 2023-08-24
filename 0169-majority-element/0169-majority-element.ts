e38f98ecad5becbb8b267ea2d43bec84ce8a402a
a. octavia <auliaoctaviani77@student.uns.ac.id>
2023-08-24 16:49:11 +0700

    Time: 57 ms (88.72%), Space: 46 MB (52.78%) - LeetHub

--- 0169-majority-element/0169-majority-element.ts
+++ 0169-majority-element/0169-majority-element.ts
@@ -1,14 +1,14 @@
 function majorityElement(nums: number[]): number {
-    let x;
-    let y = 0;
+    let x; // mendefinisikan x
+    let y = 0; // mendefinisikan nilai y = 0 
     
-    for (const num of nums) {
-        if (y === 0) {
-            x = num;
+    for (const num of nums) { // menggunakan perulangan sebanyak num
+        if (y === 0) { // kalo y adalah 0
+            x = num; // maka nilai x adalah sama seperti num
         }
         
-        y += (num === x) ? 1 : -1
+        y += (num === x) ? 1 : -1 // pengubahan nilai y akan ditambah hasil dari operasi yang berada didalam kurung , apakah 1 atau -1
     }
     
-    return x;
+    return x; // dikembalikan hasilnya x 
 };
