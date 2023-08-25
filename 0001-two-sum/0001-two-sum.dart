f0810e5b5157947cd304178bce03a7f8fd6ce8c0
a. octavia <auliaoctaviani77@student.uns.ac.id>
2023-08-25 09:19:51 +0700

    Time: 271 ms (84.83%), Space: 143.3 MB (52.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.dart
+++ 0001-two-sum/0001-two-sum.dart
@@ -1,17 +1,24 @@
 class Solution {
+    // kode berjenis list yang berisi int 
   List<int> twoSum(List<int> nums, int target) {
+    // mapping integer 
     Map<int, int> arrayMap = {};
 
+    // dengan perulangan naik panjang max nums.leght
     for (int i = 0; i < nums.length; i++) {
+        // ditampung dalam variabel complement tipe int dengan operasi
+        // target dikurangi nums yang naik
       final int complement = target - nums[i];
 
+        // jika dalam map array mengandung hasil complements 
       if (arrayMap.containsKey(complement)) {
+          // dikembalikan ini
         return [arrayMap[complement]!, i];
       }
 
       arrayMap[nums[i]] = i;
     }
 
-    return [];
+    return []; // kembalinya berbentuk array
   }
 }
