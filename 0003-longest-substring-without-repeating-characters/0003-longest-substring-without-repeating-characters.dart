3cbfc04872fbf6a6f51c7cdb5f3debbcffae1af4
a. octavia <auliaoctaviani77@student.uns.ac.id>
2023-08-25 09:29:46 +0700

    Time: 338 ms (45.06%), Space: 144.3 MB (57.85%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.dart
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.dart
@@ -1,8 +1,13 @@
 class Solution {
+  // int merupakan bentuk pengembalian  dengan inputan sebuah string 
   int lengthOfLongestSubstring(String s) {
+    // input berupa string yang diwakilkan s
     var input = s;
+    // dan letter
     Set<String> letters = {};
+    // maxSubstringnya bernilai 0
     int maxSubString = 0;
+    // 
     for (var i = 0; i < input.length; i++) {
 
       while (letters.contains(input[i])) {
