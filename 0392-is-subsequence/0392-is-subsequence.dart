b66f161766918a9c19c596510c95c1ef29a989f6
a. octavia <auliaoctaviani77@student.uns.ac.id>
2023-08-29 13:13:57 +0700

    Time: 232 ms (71.6%), Space: 140.1 MB (59.26%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.dart
+++ 0392-is-subsequence/0392-is-subsequence.dart
@@ -1,14 +1,14 @@
 class Solution {
   bool isSubsequence(String s, String t) {
-      int index = 0;
+      int index = 0; 
 
-      if(s.length > 0){
-      for(int i = 0; i < t.length; i++){
-        if(s.length > index && s[index] == t[i]){ 
+      if(s.length > 0){ // kalau panjang string lebih dari 0 maka ke step lanjutnya
+      for(int i = 0; i < t.length; i++){ // menggunakan pengulangan integer dengan batas maksimum panjang i dan pengulangan naik
+        if(s.length > index && s[index] == t[i]){ // jika panjang s lebih dari x dan index s sama dengan index i
             index++;
         }
       }
-      return s.length == index;
+      return s.length == index; // dikembalikan dalam panjang s yang berupa index
       }
       return true;
   }
