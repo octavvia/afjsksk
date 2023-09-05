d8bed1e1e565e43f04cf1c74c3c88a00f31161b2
a. octavia <auliaoctaviani77@student.uns.ac.id>
2023-09-05 15:54:58 +0700

    Time: 534 ms (59.06%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,2 +1,6 @@
 # Write your MySQL query statement below
+
+# SELECT name,population,area FROM World WHERE area >= 30000000 or population >= 25000000
+
+# Write your MySQL query statement below
 Select name,population,area from World where   population >= 25000000 or area >= 3000000
